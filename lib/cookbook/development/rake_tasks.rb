74ef32b9cf55de29ffb3bfb480bda99dba569cc8
Matt Farrar and Russ Teabeault <mfarrar@rallydev.com>
2013-07-25 10:21:12 -0600

    Refactoring rake_tasks to replace Strainer, fixing Rally foodcritic rule bug exposed by refactoring

--- lib/cookbook/development/rake_tasks.rb
+++ lib/cookbook/development/rake_tasks.rb
@@ -1,34 +1,62 @@
 require 'foodcritic'
 require 'berkshelf'
+require 'rspec/core/rake_task'
+require 'kitchen/rake_tasks'
 
-desc "All tests using Strainer and Test Kitchen"
-task :test do
-  Rake::Task['strainer'].invoke
-  Rake::Task['kitchen'].invoke
-end
+project_dir   = Dir.pwd
+chef_dir      = File.join(project_dir, '.chef')
+knife_cfg     = File.join(chef_dir, 'knife.rb')
+vendor_dir    = File.join(project_dir, 'vendor')
+cookbooks_dir = File.join(vendor_dir, 'cookbooks')
+berks_file    = File.join(project_dir, 'Berksfile')
 
-desc "Test cookbook using strainer"
-task :strainer => :berks_install do
-  puts "--> Running Strainer..."
-  sh "bundle exec strainer test"
-end
+Kitchen::RakeTasks.new
 
-desc "Run integration tests using Test Kitchen"
-task :kitchen do
-  if File.exists?(File.join(Dir.pwd, '.kitchen.yml'))
-    begin
-      require 'kitchen/rake_tasks'
-      Kitchen::RakeTasks.new
-
-      puts "--> Running Test Kitchen..."
-      Rake::Task["kitchen:all"].invoke
-    rescue LoadError
-      puts "Kitchen yml file found but unable to load test-kitchen"
-    end
+desc 'Runs knife cookbook test'
+task :knife_test => [knife_cfg, :berks_install] do |task|
+  cookbook_name = File.basename(project_dir)
+  Dir.chdir(File.join(project_dir, '..')) do
+    sh "bundle exec knife cookbook test #{cookbook_name} --config #{knife_cfg}"
   end
 end
 
-task :berks_install do
-  Berkshelf::Berksfile.from_file(File.join(Dir.pwd, 'Berksfile')).install
+desc 'Runs Foodcritic linting'
+FoodCritic::Rake::LintTask.new do |task|
+  task.options = {:search_gems => true, :epic_fail => ['any'], :exclude_paths => ['vendor/cookbooks/**/*']}
 end
 
+desc 'Runs unit tests'
+RSpec::Core::RakeTask.new(:unit) do |task|
+  task.pattern = FileList[File.join(project_dir, 'test', 'unit', '**/*_spec.rb')]
+end
+task :unit => :berks_install
+
+desc 'Runs integration tests'
+task :integration do
+  Rake::Task['kitchen:all'].invoke
+end
+
+desc 'Run all tests and linting'
+task :test do
+  FileUtils.rm_rf vendor_dir
+  Rake::Task['knife_test'].invoke
+  Rake::Task['foodcritic'].invoke
+  Rake::Task['unit'].invoke
+  Rake::Task['integration'].invoke
+end
+
+task :berks_install do |task|
+  Berkshelf::Berksfile.from_file(berks_file).install(:path => cookbooks_dir)
+end
+
+directory chef_dir
+
+file knife_cfg => chef_dir do |task|
+  File.open(task.name, 'w+') do |file|
+    file.write <<-EOF.gsub(/^\s+/, "")
+    cache_type 'BasicFile'
+    cache_options(:path => "\#{ENV['HOME']}/.chef/checksums")
+    cookbook_path '#{cookbooks_dir}'
+    EOF
+  end
+end
--- lib/foodcritic/rules/rally.rb
+++ lib/foodcritic/rules/rally.rb
@@ -3,22 +3,19 @@
 
 rule 'RALY001', 'Prefer single-quoted strings' do
   tags %w{style strings}
-  cookbook do |path|
-    recipes = Dir["#{path}/**/*.rb"]
-    recipes.collect do |recipe|
-      lines = File.readlines(recipe)
+  recipe do |ast, filename|
+    lines = File.readlines(filename)
 
-      lines.collect.with_index do |line, index|
-        # Don't flag if there is a #{} or ' in the line
-        if line.match('"(.*)"') && !line.match('\A\s?#') && !line.match('\'(.*)"(.*)"(.*)\'') && !line.match('"(.*)(#{.+}|\'|\\\a|\\\b|\\\r|\\\n|\\\s|\\\t)(.*)"')
-          {
-            :filename => recipe,
-            :matched => recipe,
-            :line => index + 1,
-            :column => 0
-          }
-        end
-      end.compact
-    end.flatten
+    lines.collect.with_index do |line, index|
+      # Don't flag if there is a #{} or ' in the line
+      if line.match('"(.*)"') && !line.match('\A\s?#') && !line.match('\'(.*)"(.*)"(.*)\'') && !line.match('"(.*)(#{.+}|\'|\\\a|\\\b|\\\r|\\\n|\\\s|\\\t)(.*)"')
+        {
+          :filename => filename,
+          :matched => recipe,
+          :line => index + 1,
+          :column => 0
+        }
+      end
+    end.compact.flatten
   end
 end
