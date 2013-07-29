64b4873c2bcc4bb353ae81bc5c30aad1d1b7741c
Eli Klein and Matt Farrar <eklein@rallydev.com>
2013-07-29 14:43:56 -0600

    Added rule to RALY001 to accommodate double-quoted strings in xml

--- lib/foodcritic/rules/rally.rb
+++ lib/foodcritic/rules/rally.rb
@@ -8,14 +8,14 @@ rule 'RALY001', 'Prefer single-quoted strings' do
 
     lines.collect.with_index do |line, index|
       # Don't flag if there is a #{} or ' in the line
-      if line.match('"(.*)"') && !line.match('\A\s?#') && !line.match('\'(.*)"(.*)"(.*)\'') && !line.match('"(.*)(#{.+}|\'|\\\a|\\\b|\\\r|\\\n|\\\s|\\\t)(.*)"')
+      if line.match('"(.*)"') && !line.match('^\s+<.+?[class|plugin]="(.+?)".*?>\s*$') && !line.match('\A\s?#') && !line.match('\'(.*)"(.*)"(.*)\'') && !line.match('"(.*)(#{.+}|\'|\\\a|\\\b|\\\r|\\\n|\\\s|\\\t)(.*)"')
         {
-          :filename => filename,
+          :filename => recipe,
           :matched => recipe,
           :line => index + 1,
           :column => 0
         }
       end
-    end.compact.flatten
-  end
+    end.compact
+  end.flatten
 end
