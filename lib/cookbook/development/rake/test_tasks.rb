ed1a4b5af9452a84982ab14f878cc3b278c3e6a6
Ali Yahya <amyahya@gmail.com>
2017-04-06 22:30:47 -0400

    Ignore some tags. (#24)

--- lib/cookbook/development/rake/test_tasks.rb
+++ lib/cookbook/development/rake/test_tasks.rb
@@ -40,7 +40,7 @@ module CookbookDevelopment
         task.options = {
           :search_gems => true,
           :fail_tags => ['any'],
-          :tags => ['~FC003', '~FC015'],
+          :tags => ['~FC003', '~FC015', '~FC064', '~FC065', '~FC066', '~FC067'],
           :exclude_paths => ['vendor/**/*']
         }
       end
